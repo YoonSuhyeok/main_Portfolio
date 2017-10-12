152f0a249961e08fe9631d226250f267d217bda8
YoonSuhyeok <cooltime15@likelion.org>
2017-10-12 13:40:45 +0900

    DB push success

--- routes/Yoon.js
+++ routes/Yoon.js
@@ -1,10 +1,39 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'guestmember',
+    database: 'mysql',
+    password: '1234'
+});
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.render('Yoon', { title: 'Yoon' });
 });
 
-module.exports = router;
+router.post('/write', function (req, res, next) {
+
+    var create_id = req.body.create_id;
+    var author = req.body.author;
+    var datas = [create_id, author];
+
+    pool.getConnection(function (err, connection) {
+
+        var sqlForInsertBoard = "insert into board(id, name) values(?,?)";
+        connection.query(sqlForInsertBoard, datas, function (err, rows) {
+            if (err) console.error("err : " + err);
+            console.log("rows : " + JSON.stringify(rows));
+
+            res.redirect('/Yoon');
+            connection.release();
+
+        });
+    });
+});
+
+module.exports = router; 
